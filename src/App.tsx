0d61f51b93812bfebff4f649b0c974f0ac3dfddc
sridhar02 <kattasridhar02@gmail.com>
2021-12-27 23:34:23 +0530

    ✨ Components and related styles

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Form } from './components/form';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Serverless TypeScript + React</h1>
+      </header>
+      <main>
+        <Form />
+      </main>
+    </>
+  );
+}
+
+export default App;
